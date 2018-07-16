13aaa857684992b933d1d2e9cdd34c85f8d46442
tiagoccs97 <39594949+tiagoccs97@users.noreply.github.com>
2018-07-16 04:19:04 -0300

    Update Ruas&Trânsito.c

--- Ruas&Trânsito.c
+++ Ruas&Trânsito.c
@@ -1,47 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int mapa[19][19];
+char mapa[19][19];
 struct carro{int x, y;};
 struct carro c[4];
 
-void print_matriz()
+
+void sinais_1()
 {
-	int i, j;
-	for(i=0; i<19; i++)
+    int i, j;
+    for(i=0; i<19; i++)
 	{
 		for(j=0; j<19; j++)
 		{
-			printf("%d ", mapa[i][j]);
+
+			if(i==6 || i==12)
+			{
+				if(j==6 || j==12 || j==0 || j==18)
+                {
+                    if(mapa[i][j]==3+48) mapa[i][j]=4+48;
+                    else if(mapa[i][j]==4+48) mapa[i][j]=3+48;
+
+				}
+			}
+
+
 		}
-		printf("\n");
 	}
-	printf("\n");
-}
-
-void car(int n)
-{
-    int k;
-    k = srand()%2;
-
-    mapa[c[n].x][c[n].y] = 'C';
-
-    if(mapa[c[n].x][c[n].y]==1)
 
 }
-
-int main()
+void sinais_2()
 {
-	int i, j;
-	int stop=0;
+    int i, j;
+    for(i=0; i<19; i++)
+	{
+		for(j=0; j<19; j++)
+		{
 
-	c[0].x = 0; c[0].y = 0;
-	c[1].x = 0; c[1].y = 18;
-	c[2].x = 18; c[2].y = 0;
-	c[3].x = 18; c[3].y = 18;
+			if(i==0 || i==18)
+			{
+				if(j==6 || j==12 || j==0 || j==18)
+                {
+                    if(mapa[i][j]==3+48) mapa[i][j]=4+48;
+                    else if(mapa[i][j]==4+48) mapa[i][j]=3+48;
 
-	// Preenche a matriz com zeros
-	for(i=0; i<19; i++)
+				}
+			}
+
+		}
+	}
+
+}
+
+void preencher_matriz()
+{
+    int i, j;
+    for(i=0; i<19; i++)
 	{
 		for(j=0; j<19; j++)
 		{
@@ -56,24 +70,90 @@ int main()
 	{
 		for(j=0; j<19; j++)
 		{
-			if((i==0 || i==6 || i==12 || i==18 || j==0 || j==6 || j==12 || j==18) && mapa[i][j]!=1) mapa[i][j]=1;
 
-			if(i==6 || i==12)
+			if(i==0 || i==12 || j==6 || j==18) mapa[i][j]=1+48;
+			if(i==6 || i==18 || j==0 || j==12) mapa[i][j]=2+48;
+
+			if(i==6 || i==12 || i==0 || i==18)
 			{
-				if(j==6 || j==12) mapa[i][j]=2;
+				if(j==6 || j==12 || j==0 || j==18) mapa[i][j]=3+48;
 			}
-			if(i==0 || i==18)
-            {
-                if(j==0 || j==18) mapa[i][j]=3;
-            }
+
 
 		}
 	}
+
+}
+
+void print_matriz()
+{
+	int i, j;
+	for(i=0; i<19; i++)
+	{
+		for(j=0; j<19; j++)
+		{
+			printf("%c ", mapa[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void car(int n)
+{
+    int k;
+    k = rand()%2;
+
+    mapa[c[n].x][c[n].y] = 'C';
+
+    if(k==0)
+    {
+        if(mapa[c[n].x][c[n].y+1]==1+48 || mapa[c[n].x][c[n].y+1]==3+48 && c[n].y+1<19) c[n].y++;
+        if(mapa[c[n].x][c[n].y-1]==2+48 || mapa[c[n].x][c[n].y-1]==3+48 && c[n].y-1>=0) c[n].y--;
+        if(mapa[c[n].x+1][c[n].y]==1+48 || mapa[c[n].x+1][c[n].y]==4+48 && c[n].x+1<19) c[n].x++;
+        if(mapa[c[n].x-1][c[n].y]==2+48 || mapa[c[n].x-1][c[n].y]==4+48 && c[n].x-1>=0) c[n].x--;
+    }
+    else
+    {
+        if(mapa[c[n].x+1][c[n].y]==1+48 || mapa[c[n].x+1][c[n].y]==4+48 && c[n].x+1<19) c[n].x++;
+        if(mapa[c[n].x-1][c[n].y]==2+48 || mapa[c[n].x-1][c[n].y]==4+48 && c[n].x-1>=0) c[n].x--;
+         if(mapa[c[n].x][c[n].y+1]==1+48 || mapa[c[n].x][c[n].y+1]==3+48 && c[n].y+1<19) c[n].y++;
+        if(mapa[c[n].x][c[n].y-1]==2+48 || mapa[c[n].x][c[n].y-1]==3+48 && c[n].y-1>=0) c[n].y--;
+    }
+
+}
+
+int main()
+{
+	int stop=0;
+	char key;
+	int count=0;
+
+	c[0].x = 0; c[0].y = 0;
+	c[1].x = 0; c[1].y = 18;
+	c[2].x = 18; c[2].y = 0;
+	c[3].x = 18; c[3].y = 18;
+
+
 	while(!stop)
     {
-        if (key[KEY_SPACE]) stop=1;
-        print_matriz();
+        scanf("%c", &key);
+        if (key==32) stop=1;
+
+        if (count<27) count++;
+        if (count==27) count=0;
+
+        preencher_matriz();
+        if (count>=5 && count<=17) sinais_1();
+        if (count>=15 && count<=26) sinais_2();
         car(0);
+        car(1);
+        car(2);
+        car(3);
+        system("cls");
+        print_matriz();
+
+
     }
 	return 0;
 }
