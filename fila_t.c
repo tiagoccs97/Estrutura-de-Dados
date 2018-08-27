f6d48ac06105938eb1a4a397fd1fa5555016b7d0
tiagoccs97 <39594949+tiagoccs97@users.noreply.github.com>
2018-08-27 00:12:05 -0300

    Update fila_t.c

--- fila_t.c
+++ fila_t.c
@@ -1,76 +1,94 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #define MAX 10
 
 struct a
 {
 	int elementos[MAX][MAX];
 	int rear[MAX], front[MAX];
-	int esperando[20];
 	int atendidos[20];
 };
 struct a q;
 
 void inserir(int x, int prioridade)
 {
-	if (q.rear[prioridade==MAX-1) q.rear[prioridade]=0;
+	if (q.rear[prioridade]==MAX-1) q.rear[prioridade]=0;
 	else q.rear[prioridade]++;
-
-	if(q.rear[prioridade]==q.front[prioridade])
+	//printf("\n%d\n%d", q.rear[prioridade], q.front[prioridade]);
+	if(q.rear[prioridade]==q.front[prioridade] && !empty(prioridade))
 	{
-		printf("overflow");
+		printf("\noverflow\n");
 		return;
 	}
-	q.elementos[prioridade][q.rear]=x;
+	q.elementos[prioridade][q.rear[prioridade]]=x;
 	return;
 }
 int remover(int prioridade)
 {
-	if (empty())
+	if (empty(prioridade))
 	{
-		printf("underflow\n");
+		printf("\nunderflow\n");
 		return 0;
 	}
 	if(q.front[prioridade]==MAX-1)
 	{
+		q.elementos[prioridade][q.front[prioridade]]=0;
 		q.front[prioridade]=0;
+
 	}
-	else q.front[prioridade]++;
-	return q.elementos[prioridade][q.front];
+	else {q.elementos[prioridade][q.front[prioridade]]=0; q.front[prioridade]++;}
+	return q.elementos[prioridade][q.front[prioridade]];
 }
 int empty(int prioridade)
 {
-	if(q.rear[prioridade]==q.front[prioridade])
+	if(q.rear[prioridade]==q.front[prioridade] && !(q.front[prioridade]-1))
 	{
 		return 1;
 	}
 	else return 0;
 }
+void print_fila(int prioridade)
+{
+	int i;
+	for (i = q.front[prioridade]+1; i != q.front[prioridade]; i++)
+	{
+		if(i>MAX-1) i=0;
+		if(q.elementos[prioridade][i]) printf("%c\n", q.elementos[prioridade][i]);
+	}
+}
+
 
 int main()
 {
-	q.rear=q.front=MAX-1;
-	int prioridade, clientes=0, i, atendidos=0;
-	for(i=0; i<4; i++) q.rear[i]=MAX-1; q.front[i]=MAX-1;  
-		while(prioridade != -1)
-		{
-			printf("Digite a prioridade do atendimento(0 a 3 ou -1 para encerrar)\n");
-			scanf("%d", &prioridade);
-			if(prioridade != -1) 
-			{
-					inserir(clientes, prioridade);
-					q.esperando[clientes]=clientes;
-					clientes++;
-			}
-		}
-		for(i=0; i<clientes; i++) printf("%c", q.esperando[clientes]+65);
-		printf("\n");
-		printf("Aperte qualquer botao para iniciar os atendimentos");
-		for(i=3; i>=0; i--) 
-		{
-				while(!empty[i])
-				{
-					q.atendidos[aten] remover(i);
+	char nome;
+	int prioridade, i, atendidos=0, tempo;
 
- 				}	
+	for(i=0; i<4; i++) {q.rear[i]=MAX-1; q.front[i]=MAX-1;}  
+	while(prioridade != -1)
+	{
+		printf("\nDigite a prioridade do atendimento e um caractere representando o cliente(0 a 3 ou -1 para encerrar)\n");
+		scanf("%d %c", &prioridade, &nome);
+		if(prioridade != -1) inserir(nome, prioridade);
+	}
+	printf("\nClientes na fila:\n\n");
+	for(i=3; i>=0; i--)   
+	{
+		print_fila(i); 		
+ 	}		
+	printf("\nEspere alguns segundos até iniciar os atendimentos\n");
+	
+	
+	if(clock()-3000)
+	{
+		printf("\nOrdem dos atendimentos(crescente)\n");
+		for(i=3; i>=0; i--) 
+		{		
+			/*while(!empty(i))
+			{
+				printf("%c\n", remover(i));
+ 			}	*/ 		
+ 	    }
+ 	}		
 	return 0;
 }
